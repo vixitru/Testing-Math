e3e357af8e89d576255e43331ceb7b8c57c8268e
vixitru <128ahanrahan@frhsd.com>
2026-09-18 12:33:26 -0400

    Finished testMultiplying

--- src/MathTest.java
+++ src/MathTest.java
@@ -42,26 +42,26 @@ public class MathTest {
        x += 1/(Math.pow(base, exponent));
        assertEquals(1/(Math.pow(base,exponent)), x);
     }
-//    @ParameterizedTest
-//    @CsvSource({
-//            "6, 5, 17",
-//            "5, 2, -3",
-//            "3, -3, -4",
-//            "-7, 2, 4",
-//            "-4, -5, 2",
-//            "-7, -8, -9",
-//            "6.3, 5.5, 17.3",
-//            "5.25, 2.4, -3.2",
-//            "3.4, -3.3, -4.2",
-//            "-7.5, 2.2, 4.6",
-//            "-4.3, -5.2, 2.1",
-//            "-7.4, -8.3, -9.2"
-//    })
-//    public void testMultiplying(double base, double a, double b) {
-//        double y = base;
-//        double exp = a+b;
-//        assertEquals((Math.pow(y,a)*Math.pow(y,b)), Math.pow(y, exp));
-//    }
+    @ParameterizedTest
+    @CsvSource({
+            "6, 5, 17",
+            "5, 2, -3",
+            "3, -3, -4",
+            "-7, 2, 4",
+            "-4, -5, 2",
+            "-7, -8, -9",
+            "6.3, 5.5, 17.3",
+            "5.25, 2.4, -3.2",
+            "3.4, -3.3, -4.2",
+            "-7.5, 2.2, 4.6",
+            "-4.3, -5.2, 2.1",
+            "-7.4, -8.3, -9.2"
+    })
+    public void testMultiplying(double base, double a, double b) {
+        double y = base;
+        double exp = a+b;
+        assertEquals((Math.pow(y,a)*Math.pow(y,b)), Math.pow(y, exp), 0.0000000001);
+    }
 
 
 }
