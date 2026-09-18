5e9c418ab1c31053817ffb12cc604a99d76807cd
vixitru <128ahanrahan@frhsd.com>
2026-09-18 12:39:48 -0400

    Created and Finished testDividing

--- src/MathTest.java
+++ src/MathTest.java
@@ -62,6 +62,26 @@ public class MathTest {
         double exp = a+b;
         assertEquals((Math.pow(y,a)*Math.pow(y,b)), Math.pow(y, exp), 0.0000000001);
     }
+    @ParameterizedTest
+    @CsvSource({
+            "6, 5, 17",
+            "5, 2, -3",
+            "3, -3, -4",
+            "-7, 2, 4",
+            "-4, -5, 2",
+            "-7, -8, -9",
+            "6.3, 5.5, 17.3",
+            "5.25, 2.4, -3.2",
+            "3.4, -3.3, -4.2",
+            "-7.5, 2.2, 4.6",
+            "-4.3, -5.2, 2.1",
+            "-7.4, -8.3, -9.2"
+    })
+    public void testDividing(double base, double a, double b) {
+       // xa / xb = xa-b
+        double subexp = a-b;
+        assertEquals((Math.pow(base,a))/(Math.pow(base,b)), Math.pow(base, subexp), 0.00000000001);
+    }
 
 
 }
