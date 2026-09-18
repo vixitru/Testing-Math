81a057acbf893ceb59fdcdbaa10cea94b10aa6d2
vixitru <128ahanrahan@frhsd.com>
2026-09-18 11:59:23 -0400

    Initial Commit

---
+++ src/MathTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class MathTest {
+
+}
